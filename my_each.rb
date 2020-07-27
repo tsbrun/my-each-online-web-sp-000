caa6f22e1575f5c334b98b51b7388ad57ebf7de9
Anya Brun <tsbrun@icloud.com>
2020-07-27 17:06:10 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -6,7 +6,7 @@ def my_each(collection)
     i = 0
 
     while i < collection.length
-      yield(collection[i])
+      yield(collection[i]) # yield first item in collection array, to block; repeat while i < collection.length
       i += 1
     end
 
